08299c6e94fe920b01a90c85b06525913b460e53
Florian Kissling <flo@6230.org>
2011-05-01 06:12:42 +0200

    Refactored fixYouTube(), made it chainable.

--- hellodirty.js
+++ hellodirty.js
@@ -294,63 +294,7 @@ g.arrange(d,b,a)}};return this.each(function(){var d=e(this),b={};b.masoned=!!d.
 columnWidth:undefined,itemSelector:undefined,appendedContent:undefined,saveOptions:true,resizeable:true,animate:false,animationOptions:{}}})(jQuery);
 
 
-function fixYouTube() {
-	/*
-		Widescreen YouTube Embeds by Matthew Buchanan & Hayden Hunter
-		http://matthewbuchanan.name/451892574
-		http://blog.haydenhunter.me
-
-		Released under a Creative Commons attribution license:
-		http://creativecommons.org/licenses/by/3.0/nz/
-	*/
-	$("div.post object").each(function () {
-		if ($(this).find("embed[src^='http://www.youtube.com']").length > 0) {
-			// Identify and hide embed(s)
-			var parent = $(this).parent();
-			parent.css("visibility","hidden");
-			var youtubeCode = parent.html();
-			var params = "";
-			if (youtubeCode.toLowerCase().indexOf("<param") == -1) {
-				// IE doesn't return params with html(), soâ€¦
-				$("param", this).each(function () {
-					params += $(this).get(0).outerHTML;
-				});
-			}
-			// Set colours in control bar to match page background
-			var oldOpts = /rel=0/g;
-			var newOpts = "rel=0&amp;color1=0x"+Whites+"&amp;color2=0x"+Whites;
-			youtubeCode = youtubeCode.replace(oldOpts, newOpts);
-			if (params != "") {
-				params = params.replace(oldOpts, newOpts);
-				youtubeCode = youtubeCode.replace(/<embed/i, params + "<embed");
-			}
-			// Extract YouTube ID and calculate ideal height
-			var youtubeIDParam = $(this).find("embed").attr("src");
-			var youtubeIDPattern = /\/v\/([0-9A-Za-z-_]*)/;
-			var youtubeID = youtubeIDParam.match(youtubeIDPattern);
-			var youtubeHeight = Math.floor(parent.find("object").width() * 0.75 + 25 - 3);
-			var youtubeHeightWide = Math.floor(parent.find("object").width() * 0.5625 + 25 - 3);
-			// Test for widescreen aspect ratio
-			$.getJSON("http://gdata.youtube.com/feeds/api/videos/" + youtubeID[1] + "?v=2&alt=json-in-script&callback=?", function (data) {
-				oldOpts = /height="?([0-9]*)"?/g;
-				if (data.entry.media$group.yt$aspectRatio != null) {
-					newOpts = 'height="' + youtubeHeightWide + '"';
-				} else {
-					newOpts = 'height="' + youtubeHeight + '"';
-				}
-				youtubeCode = youtubeCode.replace(oldOpts, newOpts);
-				if (params != "") {
-					params = params.replace(oldOpts, newOpts);
-					youtubeCode = youtubeCode.replace(/<embed/i, params + "<embed");
-				}
-				// Replace YouTube embed with new code
-				parent.html(youtubeCode).css("visibility","visible");
-			});
-		}
-	});
-}
-
-$.fn.fixYT = function() {
+$.fn.fixYouTube = function() {
 	/*
 		Widescreen YouTube Embeds by Matthew Buchanan & Hayden Hunter
 		http://matthewbuchanan.name/451892574
@@ -509,7 +453,6 @@ function fadingSidebar() {
 	// ready
 	$(function() {
 
-		fixYouTube();
 		webkitSearch();
 		fadingSidebar();
 
@@ -525,7 +468,7 @@ function fadingSidebar() {
 
 		$posts = $('#container .post');
 
-		$posts.fixVimeo().live({
+		$posts.fixYouTube().fixVimeo().live({
 			mouseenter: function(event) {
 			 $(this)
 				.addClass('active')
@@ -614,8 +557,7 @@ function fadingSidebar() {
 						}
 					});
 
-					// fix YouTube (refactor)
-					$elems.fixYT().fixVimeo();
+					$elems.fixYouTube().fixVimeo();
 
 					$elems.imagesLoaded( function(){
 					$wall.masonry({ appendedContent: $elems },
