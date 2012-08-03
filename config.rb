16f43cca46f6ec88d04c601e809559ba7c238e79
Florian Kissling <flo@6230.org>
2012-08-03 04:45:45 +0200

    Inherit h5bp's folder names.

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "css"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+images_dir = "img"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style =  :compressed #or :nested or :compact or :expanded
