02eaeff4fb9287901580e5690b001dae237bc47d
Florian Kissling <flo@6230.org>
2013-01-05 06:10:05 +0100

    Indention.

--- js/masonite.js
+++ js/masonite.js
@@ -333,52 +333,52 @@ $.fn.fixYouTube = function() {
 		Released under a Creative Commons attribution license:
 		http://creativecommons.org/licenses/by/3.0/nz/
 	*/
-		this.find("embed[src^='http://www.youtube.com']").each(function() {
-			// Identify and hide embed(s)
-			var parent = $(this).closest('object');
-			parent.css("visibility","hidden");
-			var youtubeCode = parent.html();
-			var params = "";
-			if (youtubeCode.toLowerCase().indexOf("<param") == -1) {
-				// IE doesn't return params with html(), so…
-				$("param", this).each(function () {
-					params += $(this).get(0).outerHTML;
-				});
+	this.find("embed[src^='http://www.youtube.com']").each(function() {
+		// Identify and hide embed(s)
+		var parent = $(this).closest('object');
+		parent.css("visibility","hidden");
+		var youtubeCode = parent.html();
+		var params = "";
+		if (youtubeCode.toLowerCase().indexOf("<param") == -1) {
+			// IE doesn't return params with html(), so…
+			$("param", this).each(function () {
+				params += $(this).get(0).outerHTML;
+			});
+		}
+		// Set colours in control bar to match page background
+		var oldOpts = /rel=0/g;
+		var newOpts = "rel=0&amp;color1=0x" + masonite.whites + "&amp;color2=0x" + masonite.whites;
+		youtubeCode = youtubeCode.replace(oldOpts, newOpts);
+		if (params != "") {
+			params = params.replace(oldOpts, newOpts);
+			youtubeCode = youtubeCode.replace(/<embed/i, params + "<embed");
+		}
+		// Extract YouTube ID and calculate ideal height
+		var youtubeIDParam = $(this).attr("src");
+		var youtubeIDPattern = /\/v\/([0-9A-Za-z-_]*)/;
+		var youtubeID = youtubeIDParam.match(youtubeIDPattern);
+		var youtubeHeight = Math.floor(parent.width() * 0.75 + 25 - 3);
+		var youtubeHeightWide = Math.floor(parent.width() * 0.5625 + 25 - 3);
+		// Test for widescreen aspect ratio
+		$.getJSON("http://gdata.youtube.com/feeds/api/videos/" + youtubeID[1] + "?v=2&alt=json-in-script&callback=?", function (data) {
+			oldOpts = /height="?([0-9]*)"?/g;
+			if (data.entry.media$group.yt$aspectRatio != null) {
+				newOpts = 'height="' + youtubeHeightWide + '"';
+			} else {
+				newOpts = 'height="' + youtubeHeight + '"';
 			}
-			// Set colours in control bar to match page background
-			var oldOpts = /rel=0/g;
-			var newOpts = "rel=0&amp;color1=0x" + masonite.whites + "&amp;color2=0x" + masonite.whites;
 			youtubeCode = youtubeCode.replace(oldOpts, newOpts);
 			if (params != "") {
 				params = params.replace(oldOpts, newOpts);
 				youtubeCode = youtubeCode.replace(/<embed/i, params + "<embed");
 			}
-			// Extract YouTube ID and calculate ideal height
-			var youtubeIDParam = $(this).attr("src");
-			var youtubeIDPattern = /\/v\/([0-9A-Za-z-_]*)/;
-			var youtubeID = youtubeIDParam.match(youtubeIDPattern);
-			var youtubeHeight = Math.floor(parent.width() * 0.75 + 25 - 3);
-			var youtubeHeightWide = Math.floor(parent.width() * 0.5625 + 25 - 3);
-			// Test for widescreen aspect ratio
-			$.getJSON("http://gdata.youtube.com/feeds/api/videos/" + youtubeID[1] + "?v=2&alt=json-in-script&callback=?", function (data) {
-				oldOpts = /height="?([0-9]*)"?/g;
-				if (data.entry.media$group.yt$aspectRatio != null) {
-					newOpts = 'height="' + youtubeHeightWide + '"';
-				} else {
-					newOpts = 'height="' + youtubeHeight + '"';
-				}
-				youtubeCode = youtubeCode.replace(oldOpts, newOpts);
-				if (params != "") {
-					params = params.replace(oldOpts, newOpts);
-					youtubeCode = youtubeCode.replace(/<embed/i, params + "<embed");
-				}
-				// Replace YouTube embed with new code
-				parent.html(youtubeCode).css("visibility","visible");
-			});
-
+			// Replace YouTube embed with new code
+			parent.html(youtubeCode).css("visibility","visible");
 		});
 
-		return this;
+	});
+
+	return this;
 }
 
 $.fn.fixVimeo = function() {
@@ -470,7 +470,6 @@ $.fn.fixTumblrAudio = function() {
 
 	return this;
 }
-
 
 function webkitSearch() {
 	var defaultValue = "Search"; // Default Value
